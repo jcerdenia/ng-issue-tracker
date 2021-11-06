d1009301f8d72fe8ae7eb0b050b48c6bc5d3c785
Joshua Cerdenia <joshcerdenia@gmail.com>
2021-11-06 12:03:34 +0800

    Add issue report component with layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,18 @@ import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IssueListComponent } from './issue-list/issue-list.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { IssueReportComponent } from './issue-report/issue-report.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    IssueListComponent
-  ],
+  declarations: [AppComponent, IssueListComponent, IssueReportComponent],
   imports: [
     BrowserModule,
-    ClarityModule, 
-    BrowserAnimationsModule
+    ClarityModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/issue-report/issue-report.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-issue-report',
+  templateUrl: './issue-report.component.html',
+  styleUrls: ['./issue-report.component.css'],
+})
+export class IssueReportComponent implements OnInit {
+  // FormGroup is used to group individual controls into a
+  // logical representation of a form.
+  issueForm: FormGroup | undefined;
+
+  constructor(private builder: FormBuilder) {}
+
+  ngOnInit(): void {
+    // FormBuilder.group method is used to build the form.
+    // Each key is a unique name of a form control,
+    // each value an array that contains a default value.
+    // So, this will create a new empty issue.
+    this.issueForm = this.builder.group({
+      title: [''],
+      description: [''],
+      priority: [''],
+      type: [''],
+    });
+  }
+}
